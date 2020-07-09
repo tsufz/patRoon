b40f315aae42d459c5bae14b759ac2e3b56761e9
Rick Helmus <r.helmus@uva.nl>
2020-07-09 10:17:10 +0200

    assertSets()

--- R/utils-checkmate.R
+++ R/utils-checkmate.R
@@ -331,6 +331,8 @@ assertPListIsolatePrecParams <- function(x, .var.name = checkmate::vname(x), add
     assertVal(checkmate::assertCount, "maxGap", positive = TRUE)
 }
 
+assertSets <- function(obj, s, empty.ok = TRUE, add = NULL) checkmate::assertSubset(s, sets(obj), empty.ok = empty.ok, add = add)
+
 # from https://github.com/mllg/checkmate/issues/115
 aapply = function(fun, formula, ..., fixed = list())
 {
